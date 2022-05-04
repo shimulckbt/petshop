41bf67b1489b747ab40808d7e2863e6057b347ae
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-05 02:08:53 +0600

    login functionality

--- petshop/app/Http/Controllers/Auth/LoginController.php
+++ petshop/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -28,6 +31,17 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected function redirectTo()
+    {
+        if (Auth()->user()->role_id == 1) {
+            return route('admin.dasboard');
+        } elseif (Auth()->user()->role_id == 2) {
+            return route('seller.dasboard');
+        } elseif (Auth()->user()->role_id == 3) {
+            return route('user.dasboard');
+        }
+    }
+
     /**
      * Create a new controller instance.
      *
@@ -36,5 +50,27 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))) {
+            if (auth()->user()->role_id == 1) {
+                return redirect()->route('admin.dashboard');
+            } elseif (auth()->user()->role_id == 2) {
+                return redirect()->route('seller.dashboard');
+            } elseif (auth()->user()->role_id == 3) {
+                return redirect()->route('user.dashboard');
+            } else {
+                return redirect()->route('login')->with('error', 'Email and password are wrong');
+            }
+        }
     }
 }
