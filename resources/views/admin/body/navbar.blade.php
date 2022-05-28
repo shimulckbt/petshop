9fef203e8d3b2dda312c0ad63037839884598694
Shimul Chakraborty <shimul.ckbt@gmail.com>
2022-05-28 17:59:06 +0600

    extra content removed

--- resources/views/admin/body/navbar.blade.php
+++ resources/views/admin/body/navbar.blade.php
@@ -149,8 +149,8 @@
       <!-- Nav Item - User Information -->
       <li class="nav-item dropdown no-arrow">
          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->first_name." ".Auth::user()->last_name }}</span>
-            <img class="img-profile rounded-circle" src="{{asset('images/best-seller.jpg')}}">
+            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->first_name}}</span>
+            <img class="img-profile rounded-circle" src="{{asset('images/admin.jpg')}}">
          </a>
          <!-- Dropdown - User Information -->
          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
